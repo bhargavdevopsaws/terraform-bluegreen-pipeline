f842aa6b761b140ccf0d4e9b7525f2422a2367f6
bhargavdevopsaws <158123317+bhargavdevopsaws@users.noreply.github.com>
2025-07-18 12:13:45 +0530

    Update backend.tf

--- green/backend.tf
+++ green/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-eks-cicd-4583"   
-    key            = "bluegreen/terraform.tfstate" 
+    key            = "green/terraform.tfstate" 
     region         = "ap-south-1"              
   }
 }
